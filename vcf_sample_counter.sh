9fa0d7a6464842005359c3847d3c5434b97e2bb9
Gith-gnard <toma.guignard@laposte.net>
2019-08-27 18:21:14 +0200

    Count Homo and Heterozygotes

--- vcf_sample_counter.sh
+++ vcf_sample_counter.sh
@@ -6,15 +6,27 @@ echo "usage : this_script.sh   myVCF.vcf "
 
 echo "it will output db.vcf"
 
-
-awk 'BEGIN{found=0;samplesFound="";OFS="\t"}
-{if ($0 ~ /^#/){print;if($0 ~ /^#CHROM/){split($0,genotype,"\t") }
+awk -F "\t" 'BEGIN{found=0;samplesFound="";HMZ=0;HTZ=0;OFS="\t"}
+{if ($0 ~ /^#/){print;if($0 ~ /^#CHROM/){split($0,genotype,"\t")} 
 	
 	}else{for (i=10;i<=NF; i++){
-			if($i ~ /^0\/1:/ || $i ~ /^1\// ){
-				found++;samplesFound=genotype[i]"/"samplesFound};
-			};
-		$8="found="found";samplesFound="samplesFound";"$8; print $0;found=0;samplesFound=""          
+			if($i ~ /^0\/1:/ ){
+				found++;
+				HTZ++;
+				samplesFound=genotype[i]"/"samplesFound;
+			}else if($i ~ /^1\// ){
+				found++;
+				HMZ++;
+				samplesFound=genotype[i]"/"samplesFound;
+			}
+			
+		}
+		$8="found="found";HTZ="HTZ";HMZ="HMZ";samplesFound="samplesFound";"$8;
+		print $0;
+		found=0;
+		samplesFound="";
+		HMZ=0;
+		HTZ=0;          
 	} 
 
 }' $1 > db.vcf
